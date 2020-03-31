e752e117978b20de419928376ee36fe74e5efc5a
lucreciafdez <lfernandez4@uc.cl>
2020-03-30 22:53:14 -0300

    arreglo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.active_storage.service = :amazon
+
+
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
@@ -61,6 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "site_production"
 
   config.action_mailer.perform_caching = false
+
+  config.active_storage.service = :amazon
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

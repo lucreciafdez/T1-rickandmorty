78498e679c758f1f9049b2c98296bdaf9ca492a9
lucreciafdez <lfernandez4@uc.cl>
2020-03-30 23:46:47 -0300

    am

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.active_storage.service = :amazon
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
